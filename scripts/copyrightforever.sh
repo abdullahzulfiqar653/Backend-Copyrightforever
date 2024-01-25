d092acc1138a08c00207acd9a6d6ec55309fdcca
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-01-25 21:22:31 +0500

    .sh updated

--- scripts/copyrightforever.sh
+++ scripts/copyrightforever.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
 NAME="copyright_dev_server"
-DJANGODIR=/home/dev/project/Backend-Copyrightforever
+DJANGODIR=/home/dev/project/Backend-Copyrightforever/src
 VENVPATH=/home/dev/.cache/pypoetry/virtualenvs/backend-copyrightforever--UqOecWW-py3.11
 SOC=//127.0.0.1:3005  # we will communicte using this unix socket
 USER=dev                                        # the user to run as
 GROUP=dev                                     # the group to run as
 NUM_WORKERS=3                                     # how many worker processes s>
-DJANGO_SETTINGS_MODULE=src.copyrightforever.settings             # which settings file >
-DJANGO_WSGI_MODULE=src.copyrightforever.wsgi
+DJANGO_SETTINGS_MODULE=copyrightforever.settings  # which settings file >
+DJANGO_WSGI_MODULE=copyrightforever.wsgi
 
 echo "Starting $NAME as `whoami`"
 
@@ -19,9 +18,6 @@ cd $DJANGODIR
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 export DJANGO_READ_DOT_ENV_FILE=True
-# Create the run directory if it doesn'
-# Start your Django Unicorn
-# Programs meant to be run under supervisor should not daemonize themselves (do>
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
